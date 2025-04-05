fd4f4753d26d6467fa538713399ce9c5e0dbfdf2
BobHell0 <anujan.cenan@gmail.com>
2025-04-05 20:50:11 +1100

    helloworld

---
+++ helloworld.s
@@ -0,0 +1,11 @@
+.data
+hello:      .asciiz "Hello world!\n"
+
+.text
+main:
+    li      $v0, 4          # load immeidate, v0 = 4 (4 is the printing)
+    la      $a0, hello      # load address of string to print into a0
+    syscall
+
+    li      $v0, 10         # exit syscall
+    syscall
