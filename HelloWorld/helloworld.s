a0519b7f0fc8d2356c71caa77371dc2d479bcb5c
Anujan Cenan <102663926+BobHell0@users.noreply.github.com>
2025-05-21 13:08:11 +1000

    Corrected spelling of immediate

--- HelloWorld/helloworld.s
+++ HelloWorld/helloworld.s
@@ -3,7 +3,7 @@ hello:      .asciiz "Hello world!\n"
 
 .text
 main:
-    li      $v0, 4          # load immeidate, v0 = 4 (4 is the printing)
+    li      $v0, 4          # load immediate, v0 = 4 (4 is the printing)
     la      $a0, hello      # load address of string to print into a0
     syscall
 
